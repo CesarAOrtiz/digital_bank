539b176a8c2204c4dcbad40cab4e4f43b532a70e
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2026-03-16 05:17:44 -0400

    graphql playground agregado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { ClientsModule } from './clients/clients.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { ExchangeRatesModule } from './exchange-rates/exchange-rates.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [ClientsModule, AccountsModule, TransactionsModule, ExchangeRatesModule],
+  imports: [
+    ClientsModule,
+    AccountsModule,
+    TransactionsModule,
+    ExchangeRatesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
+      playground: true,
+      // graphiql: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
